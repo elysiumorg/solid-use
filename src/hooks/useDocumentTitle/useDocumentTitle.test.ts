85254d469dfd94fe6ff663248d004921767e1a55
Vladislav <bllxy67@gmail.com>
2024-07-02 18:50:39 +0300

    fix: test

--- src/hooks/useDocumentTitle/useDocumentTitle.test.ts
+++ src/hooks/useDocumentTitle/useDocumentTitle.test.ts
@@ -1,36 +1,38 @@
 import { renderHook, waitFor } from '@solidjs/testing-library';
 import { useDocumentTitle } from './useDocumentTitle';
 
-beforeEach(() => {
-  document.title = 'default title';
-});
+describe('useDocumentTitle', () => {
+  beforeEach(() => {
+    document.title = 'default title';
+  });
 
-it('Should use document title', () => {
-  const { result } = renderHook(useDocumentTitle);
-  const [title, setTitle] = result;
+  it('Should use document title', () => {
+    const { result } = renderHook(useDocumentTitle);
+    const [title, setTitle] = result;
 
-  expect(title).toBe('default title');
-  expect(typeof setTitle).toBe('function');
-});
+    expect(title()).toBe('default title');
+    expect(typeof setTitle).toBe('function');
+  });
 
-it('Should be set initial title', () => {
-  const { result } = renderHook(() => useDocumentTitle('title'));
+  it('Should be set initial title', () => {
+    const { result } = renderHook(() => useDocumentTitle('title'));
 
-  waitFor(() => expect(result[0]).toBe('title'));
-});
+    waitFor(() => expect(result[0]).toBe('title'));
+  });
 
-it('Should be set new title', () => {
-  const { result } = renderHook(useDocumentTitle);
+  it('Should be set new title', () => {
+    const { result } = renderHook(useDocumentTitle);
 
-  result[1]('new title');
-  waitFor(() => expect(result[0]).toBe('new title'));
-});
+    result[1]('new title');
+    waitFor(() => expect(result[0]).toBe('new title'));
+  });
 
-it('Should be restore initial title when unmount', () => {
-  const { result } = renderHook(() =>
-    useDocumentTitle('title', { restoreOnUnmount: true }),
-  );
+  it('Should be restore initial title when unmount', () => {
+    const { result } = renderHook(() =>
+      useDocumentTitle('title', { restoreOnUnmount: true }),
+    );
 
-  result[1]('new title');
-  waitFor(() => expect(result[0]).toBe('new title'));
+    result[1]('new title');
+    waitFor(() => expect(result[0]).toBe('new title'));
+  });
 });
